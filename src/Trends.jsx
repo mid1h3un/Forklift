8d71b29dcccb4b49953aa62285027582b3f8302e
MIDHUN V <106835220+mid1h3un@users.noreply.github.com>
2026-01-08 11:10:06 +0530

    Replace Forklift T1 with Forklift D1

--- src/Trends.jsx
+++ src/Trends.jsx
@@ -17,7 +17,7 @@ const ForkliftTrends = () => {
     { name: "Forklift T9", imei: "865931084963206" },
     { name: "Forklift T7", imei: "865931084970326" },
     { name: "Forklift T4", imei: "865931084979863" },
-    { name: "Forklift T1", imei: "865931084970615" }
+    { name: "Forklift D1", imei: "865931084970615" }
   ];
 
   const availableTags = [];
