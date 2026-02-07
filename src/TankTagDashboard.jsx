2ccc88d3fa1bc3a5b16d578839f6e4a51fa1c468
MIDHUN V <106835220+mid1h3un@users.noreply.github.com>
2026-02-07 16:01:42 +0530

    Update Forklift D1 IMEI in TankTagDashboard

--- src/TankTagDashboard.jsx
+++ src/TankTagDashboard.jsx
@@ -7,7 +7,7 @@ const ForkliftDashboard = () => {
     { name: "Forklift T5", imei: "867512077469365" },
     { name: "Forklift T9", imei: "865931084963206" },
     { name: "Forklift T7", imei: "865931084970326" },
-    { name: "Forklift D1", imei: "865931084979863" },
+    { name: "Forklift D1", imei: "867512077492508" },
     { name: "Forklift T4", imei: "865931084970615" },
     { name: "Forklift L11", imei: "862774080074088" },
     { name: "Forklift L12", imei: "862774080073668" },
