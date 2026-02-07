b0b3b71a015a460e06f955defc1a2d4357334714
MIDHUN V <106835220+mid1h3un@users.noreply.github.com>
2026-02-07 12:37:25 +0530

    Add Forklift L12 to the dashboard data

--- src/TankTagDashboard.jsx
+++ src/TankTagDashboard.jsx
@@ -9,7 +9,8 @@ const ForkliftDashboard = () => {
     { name: "Forklift T7", imei: "865931084970326" },
     { name: "Forklift D1", imei: "865931084979863" },
     { name: "Forklift T4", imei: "865931084970615" },
-    { name: "Forklift L11", imei: "862774080074088" }
+    { name: "Forklift L11", imei: "862774080074088" },
+    { name: "Forklift L12", imei: "862774080073668" }
   ]), []);
 
   const [selected, setSelected] = useState("");
