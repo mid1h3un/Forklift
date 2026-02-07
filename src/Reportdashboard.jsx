01f0521a2b7ad7e19911c808d49aa129ba895fcb
MIDHUN V <106835220+mid1h3un@users.noreply.github.com>
2026-02-07 16:15:10 +0530

    Update D1 IMEI

--- src/Reportdashboard.jsx
+++ src/Reportdashboard.jsx
@@ -24,7 +24,7 @@ export default function ForkliftTracker() {
     { name: "Forklift T5", imei: "867512077469365", key: "t5" },
     { name: "Forklift T9", imei: "865931084963206", key: "t9" },
     { name: "Forklift T7", imei: "865931084970326", key: "t7" },
-    { name: "Forklift D1", imei: "865931084979863", key: "d1" },
+    { name: "Forklift D1", imei: "867512077492508", key: "d1" },
     { name: "Forklift T4", imei: "865931084970615", key: "t4" },
     { name: "Forklift L11", imei: "862774080074088", key: "l11" },
     { name: "Forklift L12", imei: "862774080073668", key: "l12" },
@@ -671,3 +671,4 @@ export default function ForkliftTracker() {
     </div>
   );
 }
+
