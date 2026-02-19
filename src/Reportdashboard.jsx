118ce589d5e8abf0bbd6d2d3cf6150191b428839
MIDHUN V <106835220+mid1h3un@users.noreply.github.com>
2026-02-19 16:47:00 +0530

    changed imei of D4

--- src/Reportdashboard.jsx
+++ src/Reportdashboard.jsx
@@ -33,7 +33,7 @@ export default function ForkliftTracker() {
     { name: "Forklift L11", imei: "862774080074088", key: "l11" },
     { name: "Forklift L12", imei: "862774080073668", key: "l12" },
     { name: "Forklift T8", imei: "862774080051581", key: "t8" },
-    { name: "Forklift D4", imei: "862774080074161", key: "d4" },
+    { name: "Forklift D4", imei: "867512077492508", key: "d4" },
     { name: "Forklift T1", imei: "862774080072280", key: "t1" }
   ];
 
@@ -1175,3 +1175,4 @@ export default function ForkliftTracker() {
     </div>
   );
 }
+
